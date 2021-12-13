91649cca8ec83c8a407d50f8334ef174ae5149dc
Silvia-vcp <57897967+Silvia-vcp@users.noreply.github.com>
2021-12-13 09:39:36 +0100

    Add files via upload

---
+++ neon_text.js
@@ -0,0 +1,60 @@
+﻿/*------------------------------------------------------------*/
+/*- textColor - Цвет текста ----------------------------------*/
+/*- textSize - Размер текста ---------------------------------*/
+/*- neonHighlight - цвет первой начала свечения --------------*/
+/*- neonHighlightColor цвет неоновой подсветки ---------------*/
+/*- neonHighlightHover - цвет неоновой подсветки при hover ---*/
+/*- neonFontHover - цвет шрифта при hover --------------------*/
+/*------------------------------------------------------------*/
+ 
+jQuery.fn.neonText = function(options){
+  var options = jQuery.extend({
+		textColor: '#ffffff',
+		textSize: '40pt', 
+		neonHighlight: '#ffffff', 
+		neonHighlightColor: '#FF00DE',
+		neonHighlightHover: '#00ffff', 
+		neonFontHover: '#ffffff' 
+	},options)
+	return this.each(function() {
+		jQuery(this).css('color', options.textColor)
+			.css('font-size', options.textSize)
+			.css('text-shadow','0 0 10px '+options.neonHighlight+',0 0 20px '+options.neonHighlight+',0 0 30px '+options.neonHighlight +',0 0 40px '+options.neonHighlightColor+',0 0 70px '+options.neonHighlightColor+',0 0 80px '+options.neonHighlightColor+',0 0 100px '+options.neonHighlightColor)
+			.hover(
+				function () {
+					jQuery(this)
+						.css('text-shadow','0 0 10px '+options.neonHighlight+',0 0 20px '+options.neonHighlight+',0 0 30px '+options.neonHighlight+',0 0 40px '+options.neonHighlightHover+',0 0 70px '+options.neonHighlightHover+',0 0 80px '+options.neonHighlightHover+',0 0 100px '+options.neonHighlightHover)
+						.css('color', options.neonFontHover);
+				},
+				function () {
+					jQuery(this)
+						.css('color', options.textColor)
+						.css('text-shadow','0 0 10px ' +options.neonHighlight + ',0 0 20px ' +options.neonHighlight + ',0 0 30px ' +options.neonHighlightColor + ',0 0 40px ' +options.neonHighlightColor +  ',0 0 70px ' +options.neonHighlightColor +  ',0 0 80px ' +options.neonHighlightColor + ',0 0 100px ' +options.neonHighlightColor)
+				}
+			);
+	});
+};
+
+$(document).ready(function(){
+    /*Стандартны параметры*/
+    $('.first_neon').neonText();
+    /*С настройкой*/
+    $('.second_neon').neonText({
+        textColor:'white',
+        textSize:'40pt',
+        neonHighlight:'white',
+        neonHighlightColor:'#008000',
+        neonHighlightHover:'#FFFF00',
+        neonFontHover:'white'
+    });
+    $('.third_neon').neonText({
+        textColor:'white',
+        textSize:'40pt',
+        neonHighlight:'white',
+        neonHighlightColor:'#00FFFF',
+        neonHighlightHover:'#7FFF00',
+        neonFontHover:'white'
+    });
+});
+
+
---
+++ utils.js
@@ -0,0 +1,34 @@
+﻿function CalcularFecha(fecha){
+    
+    var dia1 = new Date();
+    var dia2 = new Date(Date.parse(fecha));
+    
+    const arrayFecha = fecha.split("-");
+    
+    var diferencia = dia2 - dia1;
+    
+    if (diferencia > 0){
+        var dias = Math.ceil(diferencia / (1000*3600*24));
+        if (dias < 7){
+        document.write('<span class="badge rounded-pill bg-warning text-dark">¡Se acaba pronto!</span>');
+        } else {
+        document.write("Te quedan solo " + dias + " días para disfrutar de esta oferta");
+        }        
+    } else {
+        document.write('<span class="badge rounded-pill bg-danger">Oferta caducada</span>');
+    }
+}
+
+function calcularValoracion(){
+    let valUno = parseInt(document.getElementById("valUno").textContent);
+    let valDos = parseInt(document.getElementById("valDos").textContent);
+    let valTres = parseInt(document.getElementById("valTres").textContent);
+    
+    let valMedia = (valUno + valDos + valTres)/3;
+    
+     document.write("Valoración media: " + valMedia.toFixed(2));
+
+    
+}
+
+
